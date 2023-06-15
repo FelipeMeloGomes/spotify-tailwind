1f909d2ead29eabc4e1d4a4e9d618d6ae4d1e718
Felipe Melo <felipemelogomes36@gmail.com>
2023-06-15 18:33:28 -0300

    update: new project

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    content: [
+        "./components/**/*.{html,js}",
+        "./pages/**/*.{html,js}",
+        "./index.html",
+    ],
+    theme: {
+        extend: {
+            spacing: {
+                14: "3.5rem",
+                "125px": "125px",
+            },
+            colors: {
+                hoverspt: "#18d760",
+                "purple-main": "#2d46b9",
+                "green-main": "#1ed760",
+            },
+            backgroundImage: (theme) => ({
+                "spotify-theme": "url('../img/bursts.svg')",
+                "spotify-theme-mobile": "url('../img/bursts-mobile.svg')",
+            }),
+            backgroundSize: {
+                "175%": "175%",
+                "195%": "195%",
+            },
+            backgroundPosition: {
+                banner: "46% 4%",
+                "banner-mobile": "top 25% center",
+            },
+            fontSize: {
+                "9xl": "9rem",
+            },
+        },
+    },
+};
